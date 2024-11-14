2eeeedc2107917cf6afe0744a5913dbbc5b3306a
Ruksi Korpisara <me@ruk.si>
2024-11-14 16:09:40 +0200

    Use `valohai.inputs` to get the checkpoint dir if not specified

--- inference-mistral.py
+++ inference-mistral.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import argparse
 import logging
 
 import torch
 import transformers
+import valohai
 from peft import PeftModel
 
 from helpers import get_quantization_config
@@ -11,26 +14,31 @@ logger = logging.getLogger(__name__)
 
 
 class ModelInference:
-    def __init__(self, model_path: str, checkpoint_path: str) -> None:
+    def __init__(self, base_mistral_model: str, checkpoint_path: str | None) -> None:
+        self.checkpoint_path = checkpoint_path or valohai.inputs('finetuned-checkpoint').dir_path()
+
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(
-            model_path,
+            base_mistral_model,
             model_max_length=512,
             padding_side='left',
             add_eos_token=True,
         )
+
         logger.info('Loading model...')
         model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_path,
+            base_mistral_model,
             quantization_config=get_quantization_config(),
         )
+
         logger.info('Creating PEFT model...')
-        self.ft_model = PeftModel.from_pretrained(model, checkpoint_path).eval()
+        self.ft_model = PeftModel.from_pretrained(model, self.checkpoint_path).eval()
 
     def generate_response(self, prompt: str, max_tokens: int = 50) -> str:
         inputs = self.prepare_prompt(prompt)
         with torch.no_grad():
             logger.info('Generating up to %d tokens...', max_tokens)
             outputs = self.ft_model.generate(**inputs, max_length=max_tokens, pad_token_id=2)
+
         logger.info('Decoding...')
         return self.postprocess(self.tokenizer.decode(outputs[0], skip_special_tokens=True))
 
@@ -43,7 +51,7 @@ class ModelInference:
 
 def run(args):
     inference = ModelInference(
-        model_path=args.base_mistral_model,
+        base_mistral_model=args.base_mistral_model,
         checkpoint_path=args.checkpoint_path,
     )
     response = inference.generate_response(
@@ -59,7 +67,7 @@ def main():
     parser = argparse.ArgumentParser(description='Fine-tuned Model Inference')
     # fmt: off
     parser.add_argument('--base_mistral_model', type=str, default='mistralai/Mistral-7B-v0.1', help='Base mistral from hugging face')
-    parser.add_argument('--checkpoint_path', default='/valohai/inputs/finetuned-checkpoint/')
+    parser.add_argument('--checkpoint_path', type=str)
     parser.add_argument('--max_tokens', type=int, default=305, help='Maximum number of tokens in response')
     parser.add_argument('--prompt', type=str, required=True, help='Input prompt for text generation')
     # fmt: on
